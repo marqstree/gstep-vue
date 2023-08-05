874f93fc10d12fd2325a297c01e3058d1f79745a
hothotsavage <1725288552@qq.com>
2023-08-05 11:18:53 +0800

    新建流程

---
+++ src/util/str_util.js
@@ -0,0 +1,110 @@
+/**
+   * 计算显示的字符串
+   * @param {string} str 要裁剪的字符串
+   * @param {number} maxWidth 最大宽度
+   * @param {number} fontSize 字体大小
+   */
+export function fittingString(str, maxWidth, fontSize) {
+  var fontWidth = fontSize * 1.3; //字号+边距
+  maxWidth = maxWidth * 2; // 需要根据自己项目调整
+  var width = calcStrLen(str) * fontWidth;
+  var ellipsis = '…';
+  if (width > maxWidth) {
+    var actualLen = Math.floor((maxWidth - 10) / fontWidth);
+    var result = str.substring(0, actualLen) + ellipsis;
+    return result
+  }
+  return str
+}
+
+/**
+ * 计算字符串的长度
+ * @param {string} str 指定的字符串
+ */
+export function calcStrLen(str) {
+  var len = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128) {
+      len++;
+    } else {
+      len += 2;
+    }
+  }
+  return len;
+}
+
+export function uppercaseFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export function date2Str(datetime) {
+  let Y = datetime.getFullYear()
+  let M = (datetime.getMonth() + 1 < 10 ? '0' + (datetime.getMonth() + 1) : datetime.getMonth() + 1)
+  let D = (datetime.getDate() < 10 ? '0' + (datetime.getDate()) : datetime.getDate())
+  let h = (datetime.getHours() < 10 ? '0' + (datetime.getHours()) : datetime.getHours())
+  let m = (datetime.getMinutes() < 10 ? '0' + (datetime.getMinutes()) : datetime.getMinutes())
+  let s = (datetime.getSeconds() < 10 ? '0' + (datetime.getSeconds()) : datetime.getSeconds())
+  return `${Y}-${M}-${D} ${h}:${m}:${s}`
+}
+
+export function Number2Line(num) {
+  let str = '';
+  for (var i = 0; i < num; i++) {
+    str += '|';
+  }
+  return str;
+}
+
+//阿拉伯数字转汉字
+var chnNumChar = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+var chnUnitSection = ["", "万", "亿", "万亿", "亿亿"];
+var chnUnitChar = ["", "十", "百", "千"];
+
+export function Number2Chinese(num) {
+
+  var unitPos = 0;
+  var strIns = '', chnStr = '';
+  var needZero = false;
+
+  if (num === 0) {
+    return chnNumChar[0];
+  }
+
+  while (num > 0) {
+    var section = num % 10000;
+    if (needZero) {
+      chnStr = chnNumChar[0] + chnStr;
+    }
+    strIns = SectionToChinese(section);
+    strIns += (section !== 0) ? chnUnitSection[unitPos] : chnUnitSection[0];
+    chnStr = strIns + chnStr;
+    needZero = (section < 1000) && (section > 0);
+    num = Math.floor(num / 10000);
+    unitPos++;
+  }
+
+  return chnStr;
+}
+
+function SectionToChinese(section) {
+  var strIns = '', chnStr = '';
+  var unitPos = 0;
+  var zero = true;
+  while (section > 0) {
+    var v = section % 10;
+    if (v === 0) {
+      if (!zero) {
+        zero = true;
+        chnStr = chnNumChar[v] + chnStr;
+      }
+    } else {
+      zero = false;
+      strIns = chnNumChar[v];
+      strIns += chnUnitChar[unitPos];
+      chnStr = strIns + chnStr;
+    }
+    unitPos++;
+    section = Math.floor(section / 10);
+  }
+  return chnStr;
+}
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,159 +10,15 @@ import G6 from '@antv/g6'
 import { onMounted, ref } from 'vue';
 import Content from './widget/content.vue'
 import ApiUtil from '@/api/api'
+import { fittingString } from '@/util/str_util'
 
 const isShowDrawer = ref(false)
+const BG_PRIMARY_COLOR = "#2196f3ff"
+const BG_HOVER_COLOR = "#2196f3aa"
+const FONT_PRIMARY_COLOR = "#333333"
+const FONT_HOVER_COLOR = "#999999"
 
-var chartData = {
-  // 点集
-  nodes: [
-    {
-      id: 'node-start', // String，该节点存在则必须，节点的唯一标识
-      x: 100, // Number，可选，节点位置的 x 值
-      y: 0, // Number，可选，节点位置的 y 值
-      type: 'rect', // 矩形节点
-      label: '申请', // 矩形节点框内的文字
-      // 矩形节点框样式
-      style: {
-        fill: '#FFFFFF',
-        stroke: '#4A94FF66',
-        lineWidth: 1,
-        radius: 4
-      },
-      anchorPoints: [
-        [0.5, 0.5], [1, 0.5]
-      ]
-    },
-    {
-      id: 'node-audit', // String，该节点存在则必须，节点的唯一标识
-      x: 100, // Number，可选，节点位置的 x 值
-      y: 100, // Number，可选，节点位置的 y 值
-      type: 'rect', // 矩形节点
-      label: '审核', // 矩形节点框内的文字
-      // 矩形节点框样式
-      style: {
-        fill: '#FFFFFF',
-        stroke: '#4A94FF66',
-        lineWidth: 1,
-        radius: 4
-      },
-      anchorPoints: [
-        [0.5, 0]
-      ]
-    },
-    {
-      id: 'node-approve', // String，该节点存在则必须，节点的唯一标识
-      x: 100, // Number，可选，节点位置的 x 值
-      y: 200, // Number，可选，节点位置的 y 值
-      type: 'rect', // 矩形节点
-      label: '审批', // 矩形节点框内的文字
-      // 矩形节点框样式
-      style: {
-        fill: '#FFFFFF',
-        stroke: '#4A94FF66',
-        lineWidth: 1,
-        radius: 4
-      },
-      anchorPoints: [
-        [0.5, 0], [1, 0.5]
-      ]
-    },
-    {
-      id: 'node-end', // String，该节点存在则必须，节点的唯一标识
-      x: 100, // Number，可选，节点位置的 x 值
-      y: 300, // Number，可选，节点位置的 y 值
-      type: 'rect', // 矩形节点
-      label: '批准', // 矩形节点框内的文字
-      // 矩形节点框样式
-      style: {
-        fill: '#FFFFFF',
-        stroke: '#4A94FF66',
-        lineWidth: 1,
-        radius: 4
-      },
-      anchorPoints: [
-        [0.5, 0], [0.5, 0.5]
-      ]
-    }
-  ],
-  // 边集
-  edges: [
-    {
-      source: 'node-start', // String，必须，起始点 id
-      target: 'node-audit', // String，必须，目标点 id
-      label: '', // 边的文本
-      style: {
-        // endArrow: true,
-        // startArrow: false,
-        // 箭头样式
-        endArrow: {
-          lineDash: false,
-          path: G6.Arrow.triangle(10, 10, 2), // 使用内置箭头路径函数，参数为箭头的 宽度、长度、偏移量（默认为 0，与 d 对应）
-          d: 2,
-          fill: '#999999ff',
-          stroke: '#999999ff'
-        },
-        fill: '#99999966',
-        stroke: '#99999966'
-      }
-    },
-    {
-      source: 'node-audit', // String，必须，起始点 id
-      target: 'node-approve', // String，必须，目标点 id
-      label: '', // 边的文本
-      style: {
-        // endArrow: true,
-        // startArrow: false,
-        // 箭头样式
-        endArrow: {
-          lineDash: false,
-          path: G6.Arrow.triangle(10, 10, 2), // 使用内置箭头路径函数，参数为箭头的 宽度、长度、偏移量（默认为 0，与 d 对应）
-          d: 2,
-          fill: '#999999ff',
-          stroke: '#999999ff'
-        },
-        fill: '#99999966',
-        stroke: '#99999966'
-      }
-    },
-    {
-      source: 'node-approve', // String，必须，起始点 id
-      target: 'node-end', // String，必须，目标点 id
-      label: '', // 边的文本
-      style: {
-        // endArrow: true,
-        // startArrow: false,
-        // 箭头样式
-        endArrow: {
-          lineDash: false,
-          path: G6.Arrow.triangle(10, 10, 2), // 使用内置箭头路径函数，参数为箭头的 宽度、长度、偏移量（默认为 0，与 d 对应）
-          d: 2,
-          fill: '#999999ff',
-          stroke: '#999999ff'
-        },
-        fill: '#99999966',
-        stroke: '#99999966'
-      }
-    },
-    {
-      source: 'node-end', // String，必须，起始点 id
-      target: 'node-approve', // String，必须，目标点 id
-      label: '不合格', // 边的文本
-      type: 'polyline',
-      controlPoints: [{ x: 200, y: 300 }, { x: 200, y: 200 }],
-      style: {
-        endArrow: {
-          path: G6.Arrow.triangle(10, 10, 2),
-          d: 2,
-          fill: '#FCAD22ad',
-          stroke: '#FCAD22ad'
-        }
-      },
-      fill: '#18c298ad',
-      stroke: '#18c298'
-    }
-  ]
-}
+var chartData = {}
 
 const nodeW = 200
 const nodeH = 150
@@ -201,93 +57,103 @@ const makeChartData = () => {
       "category": "start",
       "level": 1,
       "form": {},
-      "nextSteps": []
+      "candidates": [],
+      "nextSteps": [
+        {
+          "id": 9999,
+          "title": "结束",
+          "category": "end",
+          "level": 2,
+          "form": {}
+        }
+      ]
     }
   }
 
-  step2chartData(template.rootStep)
+  formatStep(template.rootStep)
+  step2chartData(template.rootStep, null)
 }
 
-const step2chartData = (step) => {
+const formatStep = (step) => {
+  if (!step)
+    return
+
+  step.candidatesText=''
+  if (step.candidates && step.candidates.length > 0) {
+    let txt = step.candidates.map(e => e.name).join(',')
+    step.candidatesText = fittingString(txt,90,14)
+  }
+
+  if (step.nextSteps && step.nextSteps.length > 0) {
+    step.nextSteps.forEach(item => {
+      formatStep(item)
+    })
+  }
+}
+
+const step2chartData = (step, parentNode) => {
   if (!step || !step.id)
     return
 
-  let startNode = {
-    id: step.id + '', // String，该节点存在则必须，节点的唯一标识
-    x: nodeW, // Number，可选，节点位置的 x 值
-    y: nodeH * (step.level - 1), // Number，可选，节点位置的 y 值
-    // type: 'rect', // 矩形节点
-    label: '申请', // 矩形节点框内的文字
-    description: 'ant_type_name_...',
-    color: '#2196f3',
-    meta: {
-      creatorName: 'a_creator',
-    },
-    type: 'rect-jsx',
-    // 矩形节点框样式
-    style: {
-      fill: '#FFFFFF',
-      stroke: '#4A94FF66',
-      lineWidth: 1,
-      radius: 4
-    },
-    // anchorPoints: [
-    //   [0.5, 0], [0.5, 1]
-    // ]
-  }
-  chartData.nodes.push(startNode)
-
-  let endNode = {
-    id: '9999', // String，该节点存在则必须，节点的唯一标识
-    x: nodeW, // Number，可选，节点位置的 x 值
-    y: nodeH, // Number，可选，节点位置的 y 值
-    // type: 'rect', // 矩形节点
-    label: '结束', // 矩形节点框内的文字
-    description: 'ant_type_name_...',
-    color: '#2196f3',
-    meta: {
-      creatorName: 'a_creator',
-    },
-    type: 'rect-jsx',
-    // 矩形节点框样式
-    style: {
-      fill: '#FFFFFF',
-      stroke: '#4A94FF66',
-      lineWidth: 1,
-      radius: 4
-    },
-    // anchorPoints: [
-    //   [0.5, 0], [0.5, 1]
-    // ]
-  }
-  chartData.nodes.push(endNode)
-
-  let edge = {
-    source: startNode.id, // String，必须，起始点 id
-    target: endNode.id, // String，必须，目标点 id
-    label: '', // 边的文本
-    style: {
-      // endArrow: true,
-      // startArrow: false,
-      // 箭头样式
-      endArrow: {
-        lineDash: false,
-        path: G6.Arrow.triangle(10, 10, 2), // 使用内置箭头路径函数，参数为箭头的 宽度、长度、偏移量（默认为 0，与 d 对应）
-        d: 2,
-        fill: '#999999ff',
-        stroke: '#999999ff'
-      },
-      fill: '#99999966',
-      stroke: '#99999966'
+  let node = {}
+  if (step.category != 'end') {
+    node = {
+      id: step.id + '', // String，该节点存在则必须，节点的唯一标识
+      x: nodeW, // Number，可选，节点位置的 x 值
+      y: nodeH * (step.level - 1), // Number，可选，节点位置的 y 值
+      // type: 'rect', // 矩形节点
+      label: step.title, // 矩形节点框内的文字
+      detail: step,
+      bg_color: BG_PRIMARY_COLOR,
+      font_color: FONT_PRIMARY_COLOR,
+      type: 'node-start',
+      anchorPoints: [
+        [0.5, 0], [1, 0.5], [0.5, 1], [0, 0.5]
+      ]
+    }
+  } else {
+    node = {
+      id: step.id + '', // String，该节点存在则必须，节点的唯一标识
+      x: nodeW, // Number，可选，节点位置的 x 值
+      y: nodeH * (step.level - 1), // Number，可选，节点位置的 y 值
+      type: 'node-end',
+      label: '结束',
+      detail: step,
+      anchorPoints: [
+        [0.5, 0]
+      ]
     }
   }
-  chartData.edges.push(edge)
+  chartData.nodes.push(node)
+
+  if (parentNode) {
+    let edge = {
+      source: parentNode.id, // String，必须，起始点 id
+      target: node.id, // String，必须，目标点 id
+      label: '', // 边的文本
+      style: {
+        // endArrow: true,
+        // startArrow: false,
+        // 箭头样式
+        endArrow: {
+          lineDash: false,
+          path: G6.Arrow.triangle(10, 10, 2), // 使用内置箭头路径函数，参数为箭头的 宽度、长度、偏移量（默认为 0，与 d 对应）
+          d: 2,
+          fill: '#999999ff',
+          stroke: '#999999ff'
+        },
+        fill: '#99999966',
+        stroke: '#99999966'
+      }
+    }
+    chartData.edges.push(edge)
+  }
 
   if (!step.nextSteps || step.nextSteps.length < 1)
     return
 
   step.nextSteps.forEach(item => {
-    step2chartData(item)
+    step2chartData(item, node)
   })
 }
 
@@ -297,46 +163,81 @@ const setupChart = () => {
   const width = container.scrollWidth
   const height = container.scrollHeight
 
-
+  // 自定义开始节点
   G6.registerNode(
-    'rect-jsx',
+    'node-start',
     (cfg) => `
     <group>
       <rect>
         <rect style={{
           width: 150,
           height: 20,
-          fill: ${cfg.color},
+          fill: ${cfg.bg_color},
           radius: [6, 6, 0, 0],
-          cursor: 'move'，
-          stroke: ${cfg.color}
-        }} draggable="true">
+          stroke: ${cfg.bg_color},
+          cursor:'pointer'
+        }}>
           <text style={{
             marginTop: 2,
             marginLeft: 75,
             textAlign: 'center',
             fontWeight: 'bold',
-            fill: '#fff' }}>{{label}}</text>
+            fill: '#fff',
+            cursor:'pointer'
+           }} name="title-text">{{detail.title}}</text>
         </rect>
         <rect style={{
           width: 150,
-          height: 55,
-          stroke: ${cfg.color},
+          height: 40,
+          marginLeft:0,
+          stroke: ${cfg.bg_color},
           fill: #ffffff,
           radius: [0, 0, 6, 6],
+          cursor:'pointer'
         }}>
-          <text style={{ marginTop: 5, marginLeft: 3, fill: '#333', marginLeft: 4 }}>责任人: {{description}}</text>
+          <text style={{ 
+            marginTop: 5,
+            marginLeft: 5,
+            fill: ${cfg.font_color}, 
+            width:50,
+            cursor:'pointer' 
+          }} >责任人: {{detail.candidatesText}}</text>
         </rect>
       </rect>
       <circle style={{
-        stroke: ${cfg.color},
+        stroke: ${cfg.bg_color},
         r: 10,
         fill: '#fff',
         marginLeft: 75,
         cursor: 'pointer'
-      }} name="circle">
-        <image style={{ img: 'https://www.bqdnao.com/faceroop-static/add.png', width: 12, height: 12,  marginLeft: 69,  marginTop: -5 }} />
+      }} >
+        <image style={{ img: 'https://www.bqdnao.com/faceroop-static/add.png', width: 12, height: 12,  marginLeft: 69,  marginTop: -6,cursor: 'pointer' }} name="add-btn" />
       </circle>
+    </group>`,
+  )
+
+  // 自定义结束节点
+  G6.registerNode(
+    'node-end',
+    (cfg) => `
+    <group>
+      <rect>
+        <rect style={{
+          width: 150,
+          height: 25,
+          fill: '#bbb',
+          radius: [6, 6, 6, 6],
+          stroke: '#bbb'
+        }}>
+          <text style={{
+            marginTop: 3,
+            marginLeft: 75,
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fill: '#fff',
+           }} name="title-text">结束</text>
+        </rect>
+      </rect>
     </group>`,
   );
 
@@ -350,105 +251,50 @@ const setupChart = () => {
     fitView: true, // 是否将图适配到画布大小，可以防止超出画布或留白太多。
     width: width, // Number，必须，图的宽度
     height: height, // Number，必须，图的高度
+    fitCenter: true,
     size: [100, 1000],
-    //////////////////////////////////////////////////////////////////////////
-    // 交互状态 State
-    //////////////////////////////////////////////////////////////////////////
-    // 节点不同状态下的样式集合
-    nodeStateStyles: {
-      // 鼠标 hover 上节点，即 hover 状态为 true 时的样式
-      hover: {
-        stroke: "#4A94FFff",
-      },
-      // 鼠标点击节点，即 click 状态为 true 时的样式
-      click: {
-        fill: "#eeeeeeFF",
-      },
-    }
   })
-  G6.registerNode('diamond', {
-    options: {
-      style: {},
-      stateStyles: {
-        hover: {},
-        selected: {}
-      }
-    },
-    /**
-     * 绘制节点，包含文本
-     * @param  {Object} cfg 节点的配置项
-     * @param  {G.Group} group 图形分组，节点中图形对象的容器
-     * @return {G.Shape} 返回一个绘制的图形作为 keyShape，通过 node.get('keyShape') 可以获取。
-     * 关于 keyShape 可参考文档 核心概念-节点/边/Combo-图形 Shape 与 keyShape
-     */
-    draw(cfg, group) { },
-    /**
- * 绘制后的附加操作，默认没有任何操作
- * @param  {Object} cfg 节点的配置项
- * @param  {G.Group} group 图形分组，节点中图形对象的容器
- */
-    afterDraw(cfg, group) { },
-    /**
- * 更新节点，包含文本
- * @override
- * @param  {Object} cfg 节点的配置项
- * @param  {Node} node 节点
- */
-    update(cfg, node) { },
-    /**
- * 更新节点后的操作，一般同 afterDraw 配合使用
- * @override
- * @param  {Object} cfg 节点的配置项
- * @param  {Node} node 节点
- */
-    afterUpdate(cfg, node) { },
-    /**
- * 响应节点的状态变化。
- * 在需要使用动画来响应状态变化时需要被复写，其他样式的响应参见下文提及的 [配置状态样式] 文档
- * @param  {String} name 状态名称
- * @param  {Object} value 状态值
- * @param  {Node} node 节点
- */
-    setState(name, value, node) { },
-    /**
- * 获取锚点（相关边的连入点）
- * @param  {Object} cfg 节点的配置项
- * @return {Array|null} 锚点（相关边的连入点）的数组,如果为 null，则没有控制点
- */
-    getAnchorPoints(cfg) { }
-  },
-    // 继承内置节点类型的名字，例如基类 'single-node'，或 'circle', 'rect' 等
-    // 当不指定该参数则代表不继承任何内置节点类型
-    'single-node', // 基于 single-node 为例进行扩展
-  )
 
   //////////////////////////////////////////////////////////////////////////
   // 监听事件并切换元素状态
   //////////////////////////////////////////////////////////////////////////
   // 鼠标进入节点
   graph.on("node:mouseenter", (e) => {
-    const nodeItem = e.item; // 获取鼠标进入的节点元素对象
-    graph.setItemState(nodeItem, "hover", true); // 设置当前节点的 hover 状态为 true
+    const node = e.item; // 获取鼠标进入的节点元素对象
+    console.log("+++++ mouseenter +++++++++++++++++++++")
+    graph.updateItem(node, {
+      bg_color: BG_HOVER_COLOR,
+      font_color: FONT_HOVER_COLOR
+    })
+    graph.refreshItem(node)
   })
 
   // 鼠标离开节点
   graph.on("node:mouseleave", (e) => {
-    const nodeItem = e.item; // 获取鼠标离开的节点元素对象
-    graph.setItemState(nodeItem, "hover", false); // 设置当前节点的 hover 状态为 false
+    const node = e.item; // 获取鼠标离开的节点元素对象
+    console.log("+++++ mouseleave +++++++++++++++++++++")
+    graph.updateItem(node, {
+      bg_color: BG_PRIMARY_COLOR,
+      font_color: FONT_PRIMARY_COLOR
+    })
+    graph.refreshItem(node)
   })
 
-  // 点击节点
-  graph.on("node:click", (e) => {
-    // 先将所有当前是 click 状态的节点置为非 click 状态
-    const clickNodes = graph.findAllByState("node", "click");
-    console.log("Bowen: mounted -> clickNodes", clickNodes)
-    clickNodes.forEach((cn) => {
-      graph.setItemState(cn, "click", false);
-    });
-    const nodeItem = e.item; // 获取被点击的节点元素对象
-    graph.setItemState(nodeItem, "click", true); // 设置当前节点的 click 状态为 true
+  graph.on('node:click', (e) => {
+    const { item } = e
+    console.log("+++++++++ node:click ++++++++++++++++")
 
-    selectTask(nodeItem)
+    isShowDrawer.value = true
+
+    // 阻止冒泡
+    e.propagationStopped = true
+  })
+
+  graph.on('add-btn:click', (e) => {
+    const { item } = e
+    console.log("+++++++++ add-btn:click ++++++++++++++++")
+    // 阻止冒泡
+    e.propagationStopped = true
   })
 
   graph.data(chartData) // 读取 Step 2 中的数据源到图上
@@ -461,11 +307,6 @@ const onClose = () => {
   isShowDrawer.value = false
 }
 
-const selectTask = (item) => {
-  console.log(item)
-
-  isShowDrawer.value = true
-}
 </script>
 
 <style scoped>
